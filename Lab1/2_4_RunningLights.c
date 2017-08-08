6198f976bd29d7f0f59593573a10ebac40aa93db
Aaron Gardoni <aarongardoni@gmail.com>
2017-08-09 00:22:30 +1000

    Fixed running lights program

--- Lab1/2_4_RunningLights.c
+++ Lab1/2_4_RunningLights.c
@@ -11,28 +11,27 @@ unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
 int main() {
   unsigned char count = 0;
   unsigned char i = 0;
-  unsigned char prevA = 0;
   unsigned char temp = 0;
   
-  while (1) {
-    count = 0;
-    for (i = 0; i < 8; i++) {
-      count += GetBit(A, i);
-    }
+  count = 0;
+  for (i = 0; i < 8; i++) {
+    count += GetBit(A, i);
+  }
 
-    for (i = 0; i < 8; i++){
-      if (i < count) {
-        B = SetBit(B, i, 1);
-      } else {
-        B = SetBit(B, i, 0);
-      }
+  for (i = 0; i < 8; i++){
+    if (i < count) {
+      B = SetBit(B, i, 1);
+    } else {
+      B = SetBit(B, i, 0);
     }
+  }
     
-    prevA = A;
-    while (prevA == A) {
+  while (1) {
+    if (B7) {
       temp = B7;
+      B = (B << 1) + temp;
+    } else {
       B = B << 1;
-      B0 = B7;
     }
   }
 }
