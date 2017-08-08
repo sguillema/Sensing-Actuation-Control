dfdf34bce16039f027eb9349866f78d7b6380601
Hayden Crain <hayden.crain@loanflare.com>
2017-08-08 21:45:06 +1000

    Add final task into repository

---
+++ Lab1/2_5_Alarm.c
@@ -0,0 +1,32 @@
+#include "rims.h"
+
+volatile unsigned char TimerFlag = 0;
+unsigned char soundCount = 0;
+unsigned char lightCount = 0;
+
+void TimerISR() {
+      TimerFlag = 1;
+      ++soundCount;
+      ++lightCount;
+}
+
+void main()
+{
+   B = 0;
+   
+   TimerSet(50);
+   TimerOn();
+   
+   while (1) {
+      if (!(soundCount < 3)) {
+         B0 = !B0;
+         soundCount = 0;
+      }
+      if (!(lightCount < 5)) {
+         B1 = !B1;
+         lightCount = 0;
+      }
+      while(!TimerFlag) {}
+      TimerFlag = 0;
+   }
+}
