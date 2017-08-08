f5ac8972e9d8254c83c2895df86c287970d8d0c1
Aaron Gardoni <aarongardoni@gmail.com>
2017-08-08 22:33:01 +1000

    Formatted code

--- Lab1/2_1_Seatbelt.c
+++ Lab1/2_1_Seatbelt.c
@@ -1,10 +1,11 @@
 #include "RIMS.h"
 
 int main() {
-   while(1) {
-      B0 = A0 && A1 && !A2;
-   }
-   return 0;
+  while (1) {
+    B0 = A0 && A1 && !A2;
+  }
+  
+  return 0;
 }
 
 /*
--- Lab1/2_3_ParkingSensor.c
+++ Lab1/2_3_ParkingSensor.c
@@ -1,18 +1,22 @@
 #include "RIMS.h"
+
 unsigned char Getbit(unsigned char x, unsigned char k) {
    return ((x & (0x01 << k)) != 0);
 }
+
 int main() {
   unsigned char i = 0;
   unsigned char count = 0;
-   while(1) {
-     count = 0;
-      for (i = 0; i < 6; i++) {
-        count += GetBit(A, i);
-      }
-      B = ((6 - count) << 5) + (A7 && A6);
-   }
-   return 0;
+  
+  while(1) {
+    count = 0;
+    for (i = 0; i < 6; i++) {
+      count += GetBit(A, i);
+    }
+    
+    B = ((6 - count) << 5) + (A7 && A6);
+  }
+  return 0;
 }
 
 /*
