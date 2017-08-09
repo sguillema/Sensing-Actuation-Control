164b59267815ea9957cf08fdccccd38ba14e566f
aarongardoni <30831650+aarongardoni@users.noreply.github.com>
2017-08-09 12:07:17 +1000

    Update 2_4_RunningLights.c

--- Lab1/2_4_RunningLights.c
+++ Lab1/2_4_RunningLights.c
@@ -10,8 +10,7 @@ unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b)
   return (b ? (x | (0x01 << k)) : (x & ~(0x01 << k)));
 }
 
-int main()
-{
+int main() {
   unsigned char count = 0;
   unsigned char i = 0;
   unsigned char temp = 0;
