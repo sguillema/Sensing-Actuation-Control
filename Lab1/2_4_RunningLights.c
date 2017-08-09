4e29a2cb18534341c1866bf114323e9c10526b00
Aaron Gardoni <aarongardoni@gmail.com>
2017-08-09 11:28:47 +1000

    Running lights input can be changed on the fly

--- Lab1/2_4_RunningLights.c
+++ Lab1/2_4_RunningLights.c
@@ -1,37 +1,46 @@
 #include "RIMS.h"
 
-unsigned char GetBit(unsigned char x, unsigned char k) {
+unsigned char GetBit(unsigned char x, unsigned char k)
+{
   return ((x & (0x01 << k)) != 0);
 }
 
-unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
-  return (b ?  (x | (0x01 << k))  :  (x & ~(0x01 << k)) );
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b)
+{
+  return (b ? (x | (0x01 << k)) : (x & ~(0x01 << k)));
 }
 
-int main() {
+int main()
+{
   unsigned char count = 0;
   unsigned char i = 0;
   unsigned char temp = 0;
-  
-  count = 0;
-  for (i = 0; i < 8; i++) {
-    count += GetBit(A, i);
-  }
+  unsigned char prevA = 0;
 
-  for (i = 0; i < 8; i++){
-    if (i < count) {
-      B = SetBit(B, i, 1);
-    } else {
-      B = SetBit(B, i, 0);
-    }
-  }
-    
   while (1) {
-    if (B7) {
-      temp = B7;
-      B = (B << 1) + temp;
-    } else {
-      B = B << 1;
+    count = 0;
+    for (i = 0; i < 8; i++) {
+      count += GetBit(A, i);
+    }
+
+    for (i = 0; i < 8; i++) {
+      if (i < count){
+        B = SetBit(B, i, 1);
+      }
+      else{
+        B = SetBit(B, i, 0);
+      }
+    }
+
+    prevA = A;
+    while (prevA == A) {
+      if (B7) {
+        temp = B7;
+        B = (B << 1) + temp;
+      }
+      else {
+        B = B << 1;
+      }
     }
   }
 }
